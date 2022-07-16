af9a558d9a93a9bc7d657c0e60759dc2811780c8
kondelow <achmadrasidi@gmail.com>
2022-07-16 22:12:39 +0700

    fix forgot-password-route

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -20,7 +20,7 @@ const sendConfirmationEmail = async (name, email, confirmationCode) => {
       html: `<h2>Juncoffee Email Confirmation</h2>
       <h3>Hi, ${name}</h3>
       <h3>Thank you for register. Please confirm your email by clicking on the following link:</h3>
-      <a href=https://juncoffee-api.herokuapp.com//auth/confirm/${confirmationCode}> Click here to verify</a>
+      <a href=https://juncoffee-api.herokuapp.com/auth/confirm/${confirmationCode}> Click here to verify</a>
       </div>`,
     };
     await transport.sendMail(mailOptions);
