930878dc48dc5e68a3fb5401da8c0126359b2abc
kondelow <achmadrasidi@gmail.com>
2022-07-26 21:15:02 +0700

    add firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: refreshToken(process.env.OAUTH_REFRESH_TOKEN),
+});
 
 module.exports = admin;
