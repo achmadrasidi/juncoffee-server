0d84b5f17383cd0fec971bd74db5e77ced6e6add
kondelow <achmadrasidi@gmail.com>
2022-07-26 20:58:12 +0700

    add firebase config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const Router = require("express").Router();
-
+const { messaging } = require("../config/firebase");
+const notif = messaging();
 const productsRouter = require("./productRoutes");
 const usersRouter = require("./userRoutes");
 const promosRouter = require("./promoRoutes");
@@ -16,6 +17,26 @@ Router.use("/user", usersRouter);
 Router.use("/product", productsRouter);
 Router.use("/promo", promosRouter);
 Router.use("/transaction", transactionsRouter);
+Router.post("/send-notification", async (req, res) => {
+  try {
+    const { token, title, message } = req.body;
+    const msg = {
+      token,
+      notification: {
+        body: message,
+        title,
+      },
+    };
+    await notif.send(msg);
+    res.status(200).json({
+      message: "Notification Send",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
 Router.get("*", (_req, res) => {
   res.status(404).json({
     message: "Page Not Found",
